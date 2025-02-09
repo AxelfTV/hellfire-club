31656b6d4d48d7e71b49bb7a2005bbfd1e6da88a
Axelf <113626621+AxelfTV@users.noreply.github.com>
2025-02-09 22:09:27 +0000

    added headers

--- Site/assets/scripts/darkmode.js
+++ Site/assets/scripts/darkmode.js
@@ -1,11 +1,28 @@
 document.addEventListener("DOMContentLoaded", main);
 
+let darkMode;
+
 function main()
 {
-    document.getElementById("dm-toggle").addEventListener("click", toggleDarkMode);
+    if(window.localStorage.getItem("dark-mode") == null) window.localStorage.setItem("dark-mode", false);
+    darkMode = window.localStorage.getItem("dark-mode");
+    console.log("init", darkMode);
+    if(darkMode == true) toggleDarkMode();
+
+    document.getElementsByClassName("dm-toggle")[0].addEventListener("click", toggleDarkMode);
+    document.getElementsByClassName("dm-toggle")[1].addEventListener("click", toggleDarkMode);
+
+    
 }
 function toggleDarkMode() 
 {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    document.getElementById("light-header").classList.toggle("hidden");
+    document.getElementById("dark-header").classList.toggle("hidden");
+
+    darkMode = !darkMode;
+    window.localStorage.setItem("dark-mode", darkMode);
+
+    console.log(window.localStorage.getItem("dark-mode"));
 }
