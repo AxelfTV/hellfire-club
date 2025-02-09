882231ec90ca0e7d0416a59659dede88ab2f5e5d
Axelf <113626621+AxelfTV@users.noreply.github.com>
2025-02-09 21:25:24 +0000

    added simple header

--- Site/assets/scripts/darkmode.js
+++ Site/assets/scripts/darkmode.js
@@ -1,4 +1,11 @@
-function toggleDarkMode() {
+document.addEventListener("DOMContentLoaded", main);
+
+function main()
+{
+    document.getElementById("dm-toggle").addEventListener("click", toggleDarkMode);
+}
+function toggleDarkMode() 
+{
     var element = document.body;
     element.classList.toggle("dark-mode");
-  }
+}
