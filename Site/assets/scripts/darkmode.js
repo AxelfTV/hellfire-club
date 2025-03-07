a2e92cf5cb61649c52965cde8799ef7078fb69b7
Jiazi345 <97338032+Jiazi345@users.noreply.github.com>
2025-03-07 23:00:06 +0000

    add flame and particels

--- Site/assets/scripts/darkmode.js
+++ Site/assets/scripts/darkmode.js
@@ -22,8 +22,24 @@ function main()
     document.getElementsByClassName("dm-toggle")[0].addEventListener("click", toggleDarkMode);
     document.getElementsByClassName("dm-toggle")[1].addEventListener("click", toggleDarkMode);
 
-    
+
+    //add particle effects
+//only apply in certain pages
+try{
+    particlesJS.load('particles-js1', 'assets/Data/flame.json', function() {
+        console.log('Particles.js loaded - callback');
+      });
 }
+catch
+{
+    console.log("this page do not have particle effect");
+}
+//#endregion
+    loadFlame( document.getElementById("fireCanvas"));   
+    loadFlame( document.getElementById("fireCanvas2"));   
+
+}
+
 function toggleDarkMode() 
 {
     var element = document.body;
@@ -36,4 +52,39 @@ function toggleDarkMode()
     window.localStorage.setItem("dark-mode", darkMode);
 
     console.log(window.localStorage.getItem("dark-mode"));
+}
+
+function loadFlame(canvas)
+{
+
+
+const ctx = canvas.getContext("2d");
+
+canvas.width =1200;
+canvas.height = 600;
+
+const frames = [];
+const totalFrames = 24;
+let frameIndex = 0;
+const frameRate = 24;  // 目标 FPS
+const frameInterval = 1000 / frameRate;  // 计算帧间隔（毫秒）
+
+// 预加载所有帧
+for (let i = 1; i <= totalFrames; i++) {
+    let img = new Image();
+    img.src = `assets/images/flames/00${i}.png`;
+    frames.push(img);
+}
+
+// 使用 setInterval 设定帧速率
+function playAnimation() {
+    setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(frames[frameIndex], 0, 0, canvas.width, canvas.height);
+        frameIndex = (frameIndex + 1) % totalFrames;
+    }, frameInterval);
+}
+
+frames[0].onload = playAnimation;
+
 }
