b7c687481f0d4b7c0e2bd7dd29122aacdd58d9ec
Alana Prnjavorac <prnjavoa@tcd.ie>
2025-03-24 17:21:28 +0000

    Updated code
    Added content for dark and light mode about page, changed darkmode home page content sections to red instead of light green with js, and fixed gallery dark mode

--- Site/assets/scripts/darkmode.js
+++ Site/assets/scripts/darkmode.js
@@ -4,7 +4,9 @@ let darkMode;
 
 function main()
 {
-    if(window.localStorage.getItem("dark-mode") == null) window.localStorage.setItem("dark-mode", 0);
+    if(window.localStorage.getItem("dark-mode") == null) 
+        window.localStorage.setItem("dark-mode", 0);
+
     darkMode = window.localStorage.getItem("dark-mode");
     
     if(darkMode == 1) 
@@ -16,15 +18,19 @@ function main()
         document.getElementById("light-header").classList.toggle("hidden");
         document.getElementById("dark-header").classList.toggle("hidden");
         document.getElementById("particles-js1").classList.toggle("hidden");
+
+        let contentSections = document.querySelectorAll(".content-section");
+        contentSections.forEach(section => {
+            section.style.backgroundColor = "#7f1700"; // Dark red for dark mode
+        });
     }
 
 
     document.getElementsByClassName("dm-toggle")[0].addEventListener("click", toggleDarkMode);
     document.getElementsByClassName("dm-toggle")[1].addEventListener("click", toggleDarkMode);
 
-
-
 }
+
 function toggleDarkMode() 
 {
     var element = document.body;
@@ -33,6 +39,15 @@ function toggleDarkMode()
     document.getElementById("dark-header").classList.toggle("hidden");
     document.getElementById("particles-js1").classList.toggle("hidden");
 
+    let contentSections = document.querySelectorAll(".content-section");
+    contentSections.forEach(section => {
+        if (element.classList.contains("dark-mode")) {
+            section.style.backgroundColor = "#7f1700"; // Dark red in dark mode
+        } else {
+            section.style.backgroundColor = "#EEE9BF"; // Light mode color
+        }
+    });
+    
     if(darkMode == 1) darkMode = 0;
     else darkMode = 1;
     window.localStorage.setItem("dark-mode", darkMode);
